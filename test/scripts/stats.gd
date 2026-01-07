8c836847b984f9d3a2c87d879d0ad82baee2fd16
Yongjun-Kim-YJ <yongjun.j.kim@gmail.com>
2026-01-07 14:36:41 +0900

    stat_buff.gd
    Advanced Stats System in Godot! (exp, leveling, buffs, etc.)
    ~22:00

--- test/scripts/stats.gd
+++ test/scripts/stats.gd
@@ -1,6 +1,74 @@
 extends Resource
 class_name Stats
 
-@export var max_health: int = 100
-@export var defense: int = 10
-@export var attack: int = 10
+enum BuffableStats{
+	MAX_HEALTH,
+	DEFENSE,
+	ATTACK,
+}
+
+const STAT_CURVES: Dictionary[BuffableStats, Curve] = {
+	BuffableStats.MAX_HEALTH: preload("uid://c2p16xuap0acl"),
+	BuffableStats.DEFENSE: preload("uid://cl51qel72qhtc"),
+	BuffableStats.ATTACK: preload("uid://2iejt4yjdtx1"),
+}
+
+const BASE_LEVEL_XP: float = 100.0
+
+signal health_depleted
+signal health_changed(cur_health: int, max_health: int)
+
+@export var base_max_health: int = 100
+@export var base_defense: int = 10
+@export var base_attack: int = 10
+@export var experience: int = 0: set = _on_experience_set
+
+var level: int:
+	get(): return floor(max(1.0, sqrt(experience/BASE_LEVEL_XP)+0.5))
+var current_max_health: int = 100
+var current_defense: int = 10
+var current_attack: int = 10
+
+var health: int = 0: set = _on_health_set
+
+var stat_buffs: Array[StatBuff]
+
+func _init() -> void:
+	setup_stats.call_deferred()
+
+func setup_stats() -> void:
+	# recalculate the current stats first
+	recalculate_stats()
+	health = current_max_health
+
+func add_buff(buff: StatBuff) -> void:
+	stat_buffs.append(buff)
+	recalculate_stats.call_deferred()
+
+func remove_buff(buff: StatBuff) -> void:
+	stat_buffs.erase(buff)
+	recalculate_stats.call_deferred()
+
+func recalculate_stats() -> void:
+	
+	
+	var stat_sample_pos: float = (float(level)/100.0) - 0.01
+	current_max_health = base_max_health * STAT_CURVES[BuffableStats.MAX_HEALTH].sample(stat_sample_pos)
+	current_defense = base_defense * STAT_CURVES[BuffableStats.DEFENSE].sample(stat_sample_pos)
+	current_attack = base_attack * STAT_CURVES[BuffableStats.ATTACK].sample(stat_sample_pos)
+
+
+
+
+func _on_health_set(new_value: int) -> void:
+	health = clampi(new_value, 0, current_max_health)
+	health_changed.emit(health, current_max_health)
+	if health <= 0:
+		health_depleted.emit()
+
+func _on_experience_set(new_value: int) -> void:
+	var old_level: int = level
+	experience = new_value
+	
+	if not old_level == level:
+		recalculate_stats()
